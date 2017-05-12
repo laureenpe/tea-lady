44309e1526a388875bd323c5ab3deb34ca1d90e4
“Laura <laureen.pelaez.isaza@gmail.com>
2017-05-12 19:16:39 -0300

    create print function

--- js/main.js
+++ js/main.js
@@ -1,37 +1,75 @@
-/*function Customers(nombre,apellido,email,direccion,telefono,paquete){
+function Customers(nombre, apellido, email, direccion, telefono, paquete) {
     this.nombre = nombre;
     this.apellido = apellido;
     this.email = email;
     this.direccion = direccion;
     this.telefono = telefono;
-}*/
+    this.paquete = paquete;
 
-resultado = [];
-var print = document.getElementById("clientes");
-
-function mostrarData(){
-	var nombre = document.getElementById("nombre").value;
-	var apellido = document.getElementById("apellido").value;
-	var correo = document.getElementById("email").value;
-    var direccion = document.getElementById("direccion").value;
-    var telefono= document.getElementById("telefono").value;
-    var paco = document.getElementById("pack").value;
-    var resulta = 
-	resultado.push( nombre, apellido, correo, direccion,telefono,paco);
-	//document.getElementById("nombre").value = "";
-	//document.getElementById("apellido").value = "";
-	//document.getElementById("email").value = "";
-    //document.getElementById("direccion").value = "";
-    //document.getElementById("telefono").value = "";
-    /*array.forEach(function(element) {
-        element == ""
-    }, this);*/
-    print.innerHTML = ' <strong> Nombre: </strong> ' + nombre + "</br>" //imprimir
-    + '<strong> Apellido: </strong>'  + apellido + "</br>"
-    + '<strong> Email: </strong>'  +  correo + "</br>" + 
-    '<strong> Dirección: </strong>'  + direccion + "</br>" +
-    '<strong> Teléfono: </strong>' +  telefono + "</br>" +
-    '<strong>Paquete escogido  </strong>' + paco;
-	return resultado;
 }
 
+var resultado = [];
+
+function mostrarData() {
+    var nombre = document.getElementById("nombre").value;
+    var apellido = document.getElementById("apellido").value;
+    var correo = document.getElementById("email").value;
+    var direccion = document.getElementById("direccion").value;
+    var telefono = document.getElementById("telefono").value;
+    var paquete = document.getElementById("pack").value;
+    resultado.push(new Customers(nombre, apellido, correo, direccion, telefono, paquete));
+    //validacion
+    resultado.forEach(function(element) {
+        if (element.nombre == "" || element.apellido == "" || element.email == "" || element.direccion == "" || element.telefono == "" ||
+            element.paquete == "") {
+            console.log('no se encontró el elemento')
+            console.log('Nombre: ' + element.nombre);
+            console.log('Nombre: ' + element.apellido);
+            console.log('Nombre: ' + element.correo);
+            console.log('Nombre: ' + element.direccion);
+            console.log('Nombre: ' + element.telefono);
+            console.log('Nombre: ' + element.paquete);
+            alert("You must fill the content");
+            resultado.pop();
+        } else if (paquete == null || paquete == 0) {
+            alert("Select a pack");
+            resultado.pop();
+        } else {
+            console.log('todo bien!');
+            console.log('Nombre: ' + element.nombre);
+            console.log('Nombre: ' + element.apellido);
+            console.log('Nombre: ' + element.correo);
+            console.log('Nombre: ' + element.direccion);
+            console.log('Nombre: ' + element.telefono);
+            console.log('Nombre: ' + element.paquete);
+        }
+    });
+
+    /* print.innerHTML = ' <strong> Nombre: </strong> ' + nombre + "</br>" //imprimir
+         +
+         '<strong> Apellido: </strong>' + apellido + "</br>" +
+         '<strong> Email: </strong>' + correo + "</br>" +
+         '<strong> Dirección: </strong>' + direccion + "</br>" +
+         '<strong> Teléfono: </strong>' + telefono + "</br>" +
+         '<strong>Paquete escogido  </strong>' + paquete;*/
+    imprimir();
+}
+
+
+function imprimir() {
+    console.log('print!!!');
+    var print = document.getElementById("clientes");
+
+    for (var i = 0; i < resultado.length; i++) {
+        print.innerHTML = ' <strong> Nombre: </strong> ' + resultado[i].nombre + "</br>" //imprimir
+            +
+            '<strong> Apellido: </strong>' + resultado[i].apellido + "</br>" +
+            '<strong> Email: </strong>' + resultado[i].correo + "</br>" +
+            '<strong> Dirección: </strong>' + resultado[i].direccion + "</br>" +
+            '<strong> Teléfono: </strong>' + resultado[i].telefono + "</br>" +
+            '<strong>Paquete escogido  </strong>' + resultado[i].paquete;
+    }
+
+
+
+}
